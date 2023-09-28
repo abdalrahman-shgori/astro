1d9a23969854fbd19494208cb7a2900f96588f09
abd <abedoocool@gmail.com>
2023-09-28 16:53:27 +0300

    deployed website

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -13,47 +13,20 @@ import Footer from "../footer/footer";
 import Blog from "../blogs/blogs";
 
 function HomePage() {
-    const [isPageLoaded, setIsPageLoaded] = useState(false);
 
-    useEffect(() => {
-        const checkPageLoaded = () => {
-            if (document.readyState === "complete") {
-                setIsPageLoaded(true);
-            }
-        };
-
-        // Check the loading status of the document initially
-        checkPageLoaded();
-
-        // Listen for changes in the document's loading status
-        document.addEventListener("readystatechange", checkPageLoaded);
-
-        // Clean up the event listener when the component unmounts
-        return () => {
-            document.removeEventListener("readystatechange", checkPageLoaded);
-        };
-    }, []);
 
     return (
         <>
-            {!isPageLoaded ? (
-                // Display a loading indicator while the page is loading
-                <div>Loading...</div>
-            ) : (
-                // Render the components once the page is fully loaded
-                <>
-                    <StaticNav />
-                    <NavBar />
-                    <Hero />
-                    <RunWay />
-                    <Experience />
-                    <Services />
-                    <Featucres />
-                    <Talking />
-                    <Blog />
-                    <Footer />
-                </>
-            )}
+            <StaticNav />
+            <NavBar />
+            <Hero />
+            <RunWay />
+            <Experience />
+            <Services />
+            <Featucres />
+            <Talking />
+            <Blog />
+            <Footer />
         </>
     );
 }
